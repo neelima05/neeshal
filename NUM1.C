d0206af3cea879c9f1c6b5feee061c0720a72275
neelima05 <35176293+neelima05@users.noreply.github.com>
2018-01-08 00:33:49 +0530

    Add files via upload

---
+++ NUM1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,k,a[10],i,sum=0;
+printf("enter two numbers");
+scanf("%d%d",&n,&k);
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+if(n>k){
+for(i=1;i<=k;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+}
+else
+{
+printf("not valid");
+}
+}
