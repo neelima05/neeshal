7ab886c99d163bdf4eed49a891358f564dbea4ae
neelima05 <35176293+neelima05@users.noreply.github.com>
2018-01-14 01:58:53 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+void main(){
+    
+int n,t,i;
+   
+printf("enter the first interval");
+    
+scanf("%d",&n);
+  
+printf("enter the second interval");
+   
+scanf("%d",&t);
+    
+for(i=n;i<=t;i++)
+ 
+{      
+ if(i%2!=0)
+{
+       
+ printf("%d",i);
+
+}
+}
+}
